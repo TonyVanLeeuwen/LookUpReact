e023f665d30174b59e8fac2838357e995c98744e
tony <tony.van.leeuwen@fullred.nl>
2021-07-28 09:55:44 +0200

    updated observationform to use react hookform

--- src/components/Forms/CreateNewObservation/Observationform.js
+++ src/components/Forms/CreateNewObservation/Observationform.js
@@ -7,7 +7,12 @@ function ObservationForm() {
     const {handleSubmit, register} = useForm();
     const Submit = (d) =>
         axios
-            .post()
+            .post("http://localhost:8080/users/uploadobservationwithimage", {d}, {headers: {
+                'Authorization': "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjI4MjU1ODg4LCJpYXQiOjE2MjczOTE4ODh9.9ouFBf5gyOHvrtBDfY8vi3CTSBwgQgDH3AIVW-suuBs"
+                }})
+            .then(response => {
+                console.log(response)
+            })
 
     return (
         <div className="ObservationFormContainer">
